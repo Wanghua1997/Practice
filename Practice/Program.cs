268083083f6a2f1fa50604528f70901e26b4f7e0
Wanghua1997 <49910590+Wanghua1997@users.noreply.github.com>
2021-06-08 10:55:17 +0800

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[,] arr = new string[4,3] { { "小周", "5月12日","座位1" },{ "小张", "5月13日", "座位2" }, { "小平", "5月14日", "座位3" },{ "小韩", "5月15日", "座位4" } }; //定义一个二维数组
+            //Console.WriteLine(arr.GetLongLength(0));
+            //for (int i =0; i<arr.GetLongLength(0); i++) //arr.GetLongLength(0)返回数组中一维数组的个数
+            //{
+            //    Console.WriteLine("姓名:{0},入栈时间:{1},座位：{2}",arr[i,0],arr[i,1],arr[i,2]);
+            //}
+            //Console.ReadLine();
+
+            //double[] score = new double[6] {99.5,98,98.6,88,97,95};
+            //double max = score[0];
+            //double min = score[0];
+            //int Index0 = 0;
+            //int Index1 = 0;
+            //for (int i=0;i<score.Length;i++)
+            //{
+            //    if(score[i] > max)
+            //    {
+            //        max = score[i];
+            //        Index0 = i;
+            //    }
+            //    if(score[i] < min)
+            //    {
+            //        min = score[i];
+            //        Index1 = i;
+            //    }
+            //}
+            //Console.WriteLine("第{0}个元素是最大值：{1}，第{2}个元素是最小值：{3}", Index0,max,Index1,min);
+            //Console.ReadLine();
+            //随机生成一个数组并按从小到大排序
+            //void Romarray()
+            //{
+            //int temp;
+            //int[] a = new int[5];
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    a[i] = new Random().Next(1, 100);
+            //    Console.WriteLine(a[i]);
+            //}
+            //for (int i = 0; i < a.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < a.Length - 1 - i; j++)
+            //    {
+            //        if (a[j] > a[j + 1])
+            //        {
+            //            temp = a[j];
+            //            a[j] = a[j + 1];
+            //            a[j + 1] = temp;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+            //}
+            // Romarray();
+            //完成“猜数字”游戏，方法名GuessMe()：
+            //void GuessME()
+            //{
+            //    var size = new Random().Next(0, 100);
+            //    int num = 0;
+            //    Console.WriteLine("请输入一个0到100的数字");
+            //    for (int i = 0; i < 10; i++)
+            //    {
+
+            //        int size_num = Convert.ToInt16(Console.ReadLine());
+            //        num++;
+            //        if (size_num != size && size_num < size)
+            //        {
+            //            Console.WriteLine("对不起，你猜的有点小了，再试一次吧！");
+            //        }
+
+            //        else if (size_num != size && size_num > size)
+            //        {
+            //            Console.WriteLine("对不起，你猜的有大了，再试一次吧！");
+            //        }
+            //        else if (size_num == size && num < 5)
+            //        {
+            //            Console.WriteLine("恭喜你猜对了，真是天才！");
+            //        }
+            //        else if (size_num == size && num > 5 && num < 10)
+            //        {
+            //            Console.WriteLine("恭喜你猜对了，真聪明！");
+            //        }
+
+            //    }
+            //    Console.WriteLine("对不起，十次机会已用完！");
+
+            //}
+            //GuessME();
+            // 计算得到源栈同学的平均成绩（精确到两位小数），方法名GetAverage()
+
+
+        }
+      
+    }
+
+}
